421387747199d0a5c86bec8345857263bc341d5c
Will <103394466+OhDangBro@users.noreply.github.com>
2022-10-05 21:02:42 -0400

    commit before messing around

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,21 +2,33 @@ const { User, Movie } = require("../models");
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
+
+
 const resolvers = {
   Query: {
-    /// get all users ///
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('movies')
+          .populate('friends');
+    
+        return userData;
+      }
+    
+      throw new AuthenticationError('Not logged in');
+    },
     users: async () => {
       return User.find()
-        .select("-__v -password")
-        .populate("friends")
-        .populate("movieTitle");
+        .select('-__v -password')
+        .populate('movies')
+        .populate('friends');
     },
-    /// get a user by username ///
     user: async (parent, { username }) => {
       return User.findOne({ username })
-        .select("-__v -password")
-        .populate("friends")
-        .populate("movieTitle");
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
     },
     /// Get all movies ///
     movies: async (parent, { username }) => {
@@ -24,36 +36,35 @@ const resolvers = {
       return Movie.find(params).sort({ createdAt: -1 });
     },
     /// Get a movie by ID ///
-    movies: async (parent, { _id }) => {
+    movie: async (parent, { _id }) => {
       return Movie.findOne({ _id });
     },
   },
 /// Mutation Start ///
-  Mutation: {
-    /// Mutation For Adding User ///
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-    
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-    
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-    
-      const correctPw = await user.isCorrectPassword(password);
-    
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-    
-      const token = signToken(user);
-      return { token, user };
-    }
+Mutation: {
+  addUser: async (parent, args) => {
+    const user = await User.create(args);
+    const token = signToken(user);
+  
+    return { token, user };
   },
+  login: async (parent, { email, password }) => {
+    const user = await User.findOne({ email });
+  
+    if (!user) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+  
+    const correctPw = await user.isCorrectPassword(password);
+  
+    if (!correctPw) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+  
+    const token = signToken(user);
+    return { token, user };
+  }
+}
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,7 @@
 // import the gql tagged template function
 const { gql } = require("apollo-server-express");
 
+
 // create our typeDefs
 const typeDefs = gql`
   type User {
@@ -8,7 +9,7 @@ const typeDefs = gql`
     username: String
     email: String
     friendCount: Int
-    thoughts: [Movie]
+    movies: [Movie]
     friends: [User]
   }
 
@@ -18,22 +19,26 @@ const typeDefs = gql`
     createdAt: String
     username: String
   }
-
-  type Query {
-    users: [User]
-    user(username: String!): User
-    movies(username: String): [Movie]
-    movie(_id: ID!): [Movie]
-  }
-
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-  }
   type Auth {
     token: ID!
     user: User
   }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    movies(username: String): [Movie]
+    movie(_id: ID!): Movie
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    
+    
+  }
+
+ 
 `;
 
 // export the typeDefs
