7ea840d4e37c2da1dd5d861fe4a5f79391fcbb63
Will <103394466+OhDangBro@users.noreply.github.com>
2022-10-08 13:14:39 -0400

    header for movielist

--- client/src/components/MovieList/index.js
+++ client/src/components/MovieList/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import MovieListHeading from '../MovieListHeading/MovieListHeading'
 
 
 // Function component returns list of movies as props/renders div/img poster//
 const MovieList = (props) => {
     return (
-        <>
-        {/* Props for movies  */}
-        {props.movies.map((movie, index)=>
+      <>
+      <MovieListHeading heading="Movies"/>
+      <div className="row">
+      <div className="row">
+         {/* Props for movies  */}
+         {props.movies.map((movie, index)=>
         <div className="d-flex justify-content-start -m-3">
             {/* returns movie Poster from api search */}
             <img src={movie.Poster} alt="movie">
             </img>
         </div>)}
+      </div>
+      </div>
+      
+       
         </>
     )
 }
 
 export default MovieList;
+
