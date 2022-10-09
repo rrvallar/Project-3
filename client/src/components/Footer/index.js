f77d622deb435daaa40d92ff3e3f6e59f83d22af
livmeierx <liv.meier13@gmail.com>
2022-10-09 16:32:58 -0400

    styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
+    <footer className="w-100 mt-auto bg-tertiary p-4">
       <div className="container">&copy;Group 2</div>
     </footer>
   );
