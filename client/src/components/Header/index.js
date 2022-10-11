e407a4e2c09867113731b24d41793eea93497f90
livmeierx <liv.meier13@gmail.com>
2022-10-11 18:42:59 -0400

    css cleanup

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-tertiary mb-4 py-1 flex-row align-center">
+    <header className="header bg-tertiary mb-1 py-1 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <h1>Movie Match</h1>
       </div>
