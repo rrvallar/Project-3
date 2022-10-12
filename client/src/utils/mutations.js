bafc79c2d4b7677f1aeaeb39d115a110c730b276
rrvallar <rrvallar@gmail.com>
2022-10-12 19:03:03 -0400

    signup-working

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,23 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const ADD_MOVIE = gql`
-    mutation movie($id: ID!) {
-        movie(_id: $id) {   
-            _id
-            movieTitle
-            createdAt
-            username
+  mutation movie($id: ID!) {
+    movie(_id: $id) {
+      _id
+      movieTitle
+      createdAt
+      username
+    }
+  }
+`;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
