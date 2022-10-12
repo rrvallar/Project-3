24278695e7bc093e807cb95f20242391a19154c2
Kelly Wacker <kelwacker@gmail.com>
2022-10-12 00:39:54 -0400

    navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,11 +7,11 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           {/* ----------- Homenavbar section start --------------- */}
-          <li className="mx-2"><a href="./">Home</a></li>
+          <li className="mx-2"><a href="../../">Home</a></li>
           {/* ----------Profile navbar section start------------------ */}
-          <li className="mx-2 "><a href="./Profile">Profile</a></li>
+          <li className="mx-2 "><a href="../../Profile">Profile</a></li>
           {/* ----------Login navbar section start------------------ */}
-          <li className="mx-2 "><a href="./Login">Login</a></li>
+          <li className="mx-2 "><a href="../../Login">Login</a></li>
         </ul>
       </nav>
     </header>
