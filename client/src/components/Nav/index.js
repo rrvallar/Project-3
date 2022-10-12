e3c5344fad1d0c265ec12df43949d93b129dd8fc
rrvallar <rrvallar@gmail.com>
2022-10-12 19:44:29 -0400

    login-feature

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,20 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import auth from "../../utils/auth";
 
 // Nav Function Start ///
 function Nav() {
+  const logout = (e) => {
+    e.preventDefault();
+    auth.logout();
+  };
   return (
-    <header>
-      <nav>
-        <ul className="flex-row">
-          {/* ----------- Homenavbar section start --------------- */}
-          <li className="mx-2"><a href="../../">Home</a></li>
-          {/* ----------Profile navbar section start------------------ */}
-          <li className="mx-2 "><a href="../../Profile">Profile</a></li>
-          {/* ----------Login navbar section start------------------ */}
-          <li className="mx-2 "><a href="../../Login">Login</a></li>
-        </ul>
-      </nav>
-    </header>
+    // <header>
+    //   <nav>
+    //     <ul className="flex-row">
+    //       {/* ----------- Homenavbar section start --------------- */}
+    //       <li className="mx-2"><a href="../../">Home</a></li>
+    //       {/* ----------Profile navbar section start------------------ */}
+    //       <li className="mx-2 "><a href="../../Profile">Profile</a></li>
+    //       {/* ----------Login navbar section start------------------ */}
+    //       <li className="mx-2 "><a href="../../Login">Login</a></li>
+    //     </ul>
+    //   </nav>
+    // </header>
+    <nav className="flex-row">
+      {auth.loggedIn() ? (
+        <>
+          <Link to="/profile"> Profile </Link>
+          <Link to="/" onClick={logout}>
+            {" "}
+            Log out{" "}
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/">Home</Link>
+          <Link to="/Login">Log in</Link>
+        </>
+      )}
+    </nav>
   );
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import auth from "../utils/auth";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  // create login object from calling mutation
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -17,45 +22,53 @@ const Login = (props) => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      // call login and check if username and password is mathced
+      auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card bg-tertiary'>
-          <h5 className='mx-4'>Login</h5>
-          <div className='card-body'>
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-md-6">
+        <div className="card bg-tertiary">
+          <h5 className="mx-4">Login</h5>
+          <div className="card-body">
             <form onSubmit={handleFormSubmit}>
               <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
+                className="form-input"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                id="email"
                 value={formState.email}
                 onChange={handleChange}
               />
               <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                id="password"
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className='btn d-block w-100' type='submit'>
+              <button className="btn d-block w-100" type="submit">
                 Submit
               </button>
             </form>
           </div>
-          <div className='login-link'>
-            <p>Don't have an account?<br></br><a href='./Signup'>Signup Instead</a></p>
+          <div className="login-link">
+            <p>
+              Don't have an account?<br></br>
+              <a href="./Signup">Signup Instead</a>
+            </p>
           </div>
         </div>
       </div>
